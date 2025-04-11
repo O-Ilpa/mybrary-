e494534e7d6cb908e6b800b4d5b3253b52540dcd
Omar Ilpa <155574562+O-Ilpa@users.noreply.github.com>
2025-04-11 17:42:06 +0200

    file Upload fix

--- routes/book.js
+++ routes/book.js
@@ -1,19 +1,8 @@
 const express = require("express");
 const Book = require("../models/book");
 const Author = require("../models/author");
-const path = require("path");
-const uploadPath = path.join("public", Book.coverImageBasePath);
 const router = express.Router();
-const multer = require("multer");
-const fs = require("fs");
-const book = require("../models/book");
 const imageMimeTypes = ["image/jpeg", "image/png", "image/gif"];
-const upload = multer({
-  dest: uploadPath,
-  fileFilter: (req, file, callback) => {
-    callback(null, imageMimeTypes.includes(file.mimetype));
-  },
-});
 
 router.get("/", async (req, res) => {
     let searchOptions = {};
@@ -32,31 +21,30 @@ router.get("/new", async (req, res) => {
   renderNewPage(res, new Book())
 });
 
-router.post("/", upload.single("cover"), async (req, res) => {
-  const fileName = req.file != null ? req.file.filename : null;
+router.post("/", async (req, res) => {
   const book = new Book({
     title: req.body.title,
     author: req.body.author,
     publishDate: new Date(req.body.publishDate),
     pageCount: req.body.pageCount,
     description: req.body.description,
-    coverImageName: fileName,
   });
+  saveCover(book, req.body.cover)
   try {
     await book.save();
     res.redirect("books")
     console.log("book saved Succesfully")
   } catch (err) {
-    if (book.coverImageName != null) {
-      removeBookCover(book.coverImageName)
-    }
     renderNewPage(res, book, err)
   } 
 });
-const removeBookCover = (fileName) => {
-  fs.unlink(path.join(uploadPath, fileName), (err) => {
-    console.error(err)
-  })
+const saveCover = (book, coverEncoded) => {
+  if (coverEncoded == null ) return 
+  const cover = JSON.parse(coverEncoded)
+  if (cover != null && imageMimeTypes.includes(cover.type)) {
+    book.coverImage = new Buffer.from( cover.data, "base64");
+    book.coverImageType = cover.type
+  }
 }
 const renderNewPage = async (res, book, hasError) => {
   try {
